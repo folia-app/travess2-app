7f4a67534654d63261b45feb2cceb0e01b689e9c
evvvritt <evv.info@gmail.com>
2025-12-19 14:11:40 +0100

    use local iframe.html file for app. iframe.html default loads window.origin, overwrites with ?ipfspath= query param

--- src/views/TokenView.vue
+++ src/views/TokenView.vue
@@ -1,20 +1,25 @@
 <script setup>
-import { useRoute } from 'vue-router';
+import { useRoute } from 'vue-router'
 import { onMounted, onUnmounted } from 'vue'
-import { ref, computed } from 'vue';
-import { ipfsIframe } from '../plugins/ipfs';
-import store from '../store';
+import { ref, computed } from 'vue'
+// import { ipfsIframe } from '../plugins/ipfs'
+import store from '../store'
 
 const route = useRoute()
 const tokenId = route.params.tokenId
-const isNotMinted = computed(() => !store.state.nfts ? undefined : store.state.nfts.find(token => token.tokenId === tokenId) === undefined)
+const isNotMinted = computed(() =>
+  !store.state.nfts
+    ? undefined
+    : store.state.nfts.find((token) => token.tokenId === tokenId) === undefined
+)
 
-const iframePath = import.meta.env.VITE_DEV_IFRAME_PATH ?? ipfsIframe
+// const iframePath = import.meta.env.VITE_DEV_IFRAME_PATH ?? ipfsIframe
+const iframePath = '/iframe.html?ipfspath=' + encodeURIComponent(import.meta.env.VITE_IPFS_PATH)
 const { VITE_IPFS_HASH_IMAGES } = import.meta.env
 
 const loading = ref(true)
 
-function onIframeLoad (e) {
+function onIframeLoad() {
   loading.value = false
 }
 
@@ -35,12 +40,22 @@ onUnmounted(() => {
 </script>
 
 <template>
-  <article class="fixed z-30 overlay bg-[rgba(0,0,0,0.85)] overflow-hidden p-9 md:p-12 flex cursor-pointer">
-    <button ref="closeOverlayButton" class="absolute overlay outline-none" @click="$router.go(-1)" aria-label="Close token view"></button>
+  <article
+    class="fixed z-30 overlay bg-[rgba(0,0,0,0.85)] overflow-hidden p-9 md:p-12 flex cursor-pointer"
+  >
+    <button
+      ref="closeOverlayButton"
+      class="absolute overlay outline-none"
+      @click="$router.go(-1)"
+      aria-label="Close token view"
+    ></button>
     <!-- inset media from window edge -->
     <div class="relative w-full z-10" @click.stop>
       <!-- (loading) -->
-      <div class="absolute overlay flex items-center justify-center bg-black" style="font-family:monospace; font-size:14px">
+      <div
+        class="absolute overlay flex items-center justify-center bg-black"
+        style="font-family: monospace; font-size: 14px"
+      >
         <template v-if="isNotMinted === true">
           #{{ ('000' + tokenId).slice(-3) }} is not yet minted!
         </template>
@@ -50,11 +65,14 @@ onUnmounted(() => {
       </div>
 
       <template v-if="isNotMinted === false">
-        <iframe class="absolute overlay" :src="`${iframePath}#${tokenId}-${VITE_IPFS_HASH_IMAGES}`" @load="onIframeLoad" />
+        <iframe
+          class="absolute overlay"
+          :src="`${iframePath}#${tokenId}-${VITE_IPFS_HASH_IMAGES}`"
+          @load="onIframeLoad"
+        />
       </template>
     </div>
   </article>
 </template>
 
-<style scoped>
-</style>
+<style scoped></style>
